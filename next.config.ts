86eeff0b1a130fb51fff6337863edfdee000fa46
Benaun <haritonov.1991@mail.ru>
2025-09-07 12:44:11 +0300

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      '@prisma/client',
-      'prisma'
-    ]
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Исключаем Prisma из клиентского бандла
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@prisma/client': false,
-        prisma: false,
-        '@/generated/prisma': false
-      }
-    }
-    return config
-  }
+  serverExternalPackages: ['@prisma/client', 'prisma']
 }
 
 export default nextConfig
